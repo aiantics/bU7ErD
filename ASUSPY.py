4d4c6c8930a0c936913bfdd604f7574ba10a982c
aiantics <98560497+aiantics@users.noreply.github.com>
2023-10-03 23:15:20 -0700

    Update ASUSPY.py

--- ASUSPY.py
+++ ASUSPY.py
@@ -1,4 +1,5 @@
 import os, subprocess, ctypes, time
+username = os.getlogin()
 kernel32 = ctypes.WinDLL('kernel32')
 user32 = ctypes.WinDLL('user32')
 SW_HIDEN = 0
@@ -9,8 +10,19 @@ with open("C:\\Windows\\SystemApps\\XHEN\\ASUSX1.txt", "r") as file:
 with open("C:\\Windows\\SystemApps\\XHEN\\ASUSY2.txt", "r") as file:
     uuid_number = file.read().strip()
 time.sleep(0.1)
-command1 = f"C:\\Windows\\SystemApps\\XHEN\\dx11.exe /BS \"{serial_number}\""
-command2 = f"C:\\Windows\\SystemApps\\XHEN\\dx11.exe /SU \"{uuid_number}\""
+command1 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-VR90EX\\dx11.exe /BS \"{serial_number}\""
+command2 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-VR90EX\\dx11.exe /SU \"{uuid_number}\""
+command3 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-MP120G\\dx11.exe /BS \"{serial_number}\""
+command4 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-MP120G\\dx11.exe /SU \"{uuid_number}\""
+command5 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-LOP19J\\dx11.exe /BS \"{serial_number}\""
+command6 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-LOP19J\\dx11.exe /SU \"{uuid_number}\""
+time.sleep(0.1)
+subprocess.run(command1, shell=True)
+subprocess.run(command2, shell=True)
+subprocess.run(command3, shell=True)
+subprocess.run(command4, shell=True)
+subprocess.run(command5, shell=True)
+subprocess.run(command6, shell=True)
 time.sleep(0.1)
 subprocess.run(command1, shell=True)
 subprocess.run(command2, shell=True)
