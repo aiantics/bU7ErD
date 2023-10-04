02657a86ec5729435212c89a20ae792616d4487f
aiantics <98560497+aiantics@users.noreply.github.com>
2023-10-04 15:13:03 -0700

    Update ASUSPY2.py

--- ASUSPY2.py
+++ ASUSPY2.py
@@ -1,5 +1,5 @@
 import os, subprocess, ctypes, time, random
-username = os.getlogin()
+username = os.path.basename(os.path.normpath(os.path.expanduser("~")))
 kernel32 = ctypes.WinDLL('kernel32')
 user32 = ctypes.WinDLL('user32')
 SW_HIDEN = 0
@@ -8,27 +8,27 @@ user32.ShowWindow(hWnd, SW_HIDEN)
 first_digit = '2'
 remaining_digits = ''.join(str(random.randint(0, 9)) for _ in range(14))
 serial_number = first_digit + remaining_digits
-time.sleep(0.1)
-command1 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-VR90EX\\dx11.exe /BS \"{serial_number}\""
-command2 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-VR90EX\\dx11.exe /SU AUTO"
-command3 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-MP120G\\dx11.exe /BS \"{serial_number}\""
-command4 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-MP120G\\dx11.exe /SU AUTO"
-command5 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-LOP19J\\dx11.exe /BS \"{serial_number}\""
-command6 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-LOP19J\\dx11.exe /SU AUTO"
-time.sleep(0.1)
+command1 = f"C:\\Users\\\"{username}\"\\AppData\\Local\\XHEN\\D-VR90EX\\dx11.exe /BS \"{serial_number}\""
+command2 = f"C:\\Users\\\"{username}\"\\AppData\\Local\\XHEN\\D-VR90EX\\dx11.exe /SU AUTO"
+command3 = f"C:\\Users\\\"{username}\"\\AppData\\Local\\XHEN\\D-MP120G\\dx11.exe /BS \"{serial_number}\""
+command4 = f"C:\\Users\\\"{username}\"\\AppData\\Local\\XHEN\\D-MP120G\\dx11.exe /SU AUTO"
+command5 = f"C:\\Users\\\"{username}\"\\AppData\\Local\\XHEN\\D-LOP19J\\dx11.exe /BS \"{serial_number}\""
+command6 = f"C:\\Users\\\"{username}\"\\AppData\\Local\\XHEN\\D-LOP19J\\dx11.exe /SU AUTO"
 subprocess.run(command1, shell=True)
 subprocess.run(command2, shell=True)
 subprocess.run(command3, shell=True)
 subprocess.run(command4, shell=True)
 subprocess.run(command5, shell=True)
 subprocess.run(command6, shell=True)
-time.sleep(0.1)
 subprocess.run(command1, shell=True)
 subprocess.run(command2, shell=True)
-time.sleep(1.0)
 os.system("net stop winmgmt /y")
 os.system("net start winmgmt /y")
 os.system("sc stop winmgmt")
 os.system("sc start winmgmt")
-time.sleep(0.1)
+time.sleep(3)
+os.system("net stop winmgmt /y")
+os.system("net start winmgmt /y")
+os.system("sc stop winmgmt")
+os.system("sc start winmgmt")
 os.system("exit")
