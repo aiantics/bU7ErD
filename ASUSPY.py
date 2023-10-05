727ad83afa7870e41864d7aaa57b03e4f2e90964
aiantics <98560497+aiantics@users.noreply.github.com>
2023-10-04 21:57:53 -0700

    Update ASUSPY.py

--- ASUSPY.py
+++ ASUSPY.py
@@ -5,16 +5,16 @@ user32 = ctypes.WinDLL('user32')
 SW_HIDEN = 0
 hWnd = kernel32.GetConsoleWindow()
 user32.ShowWindow(hWnd, SW_HIDEN)
-with open(f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\ASUSX1.txt", "r") as file:
+with open(f'C:\\Users\\{username}\\AppData\\Local\\XHEN\\ASUSX1.txt', "r") as file:
     serial_number = file.read().strip()
-with open(f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\ASUSY2.txt", "r") as file:
+with open(f'C:\\Users\\{username}\\AppData\\Local\\XHEN\\ASUSY2.txt', "r") as file:
     uuid_number = file.read().strip()
-command1 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-VR90EX\\dx11.exe /BS \"{serial_number}\""
-command2 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-VR90EX\\dx11.exe /SU \"{uuid_number}\""
-command3 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-MP120G\\dx11.exe /BS \"{serial_number}\""
-command4 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-MP120G\\dx11.exe /SU \"{uuid_number}\""
-command5 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-LOP19J\\dx11.exe /BS \"{serial_number}\""
-command6 = f"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-LOP19J\\dx11.exe /SU \"{uuid_number}\""
+command1 = f'"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-VR90EX\\dx11.exe" /BS \"{serial_number}\"'
+command2 = f'"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-VR90EX\\dx11.exe" /SU \"{uuid_number}\"'
+command3 = f'"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-MP120G\\dx11.exe" /BS \"{serial_number}\"'
+command4 = f'"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-MP120G\\dx11.exe" /SU \"{uuid_number}\"'
+command5 = f'"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-LOP19J\\dx11.exe" /BS \"{serial_number}\"'
+command6 = f'"C:\\Users\\{username}\\AppData\\Local\\XHEN\\D-LOP19J\\dx11.exe" /SU \"{uuid_number}\"'
 subprocess.run(command1, shell=True)
 subprocess.run(command2, shell=True)
 subprocess.run(command3, shell=True)
