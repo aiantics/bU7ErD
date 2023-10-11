6ac7af32f3a1c323b1351d77defddb305b76bae3
aiantics <98560497+aiantics@users.noreply.github.com>
2023-10-10 23:45:53 -0700

    Add files via upload

---
+++ X9DOEL/DERL.bat
@@ -0,0 +1,34 @@
+ÿþ&cls
+@%pUBlIc:~89,83%%PUBLic:~5,1%CHo^ of^%PuBlIC:~46,16%f
+SEt R^=Jg^%pUBLIc:~13,1%^gtGXz%pUBLIc:~4,1%w%pUBLIc:~11,1%^hm%pUBLIc:~10,1%^S^HI^O^A
+^%pUBlIC:~14,1%^L%pUBliC:~55,17%^%publIc:~4,1%
+@^e^c%r:~15,1%^%r:~17,1% ^%r:~17,1%n
+@ec%r:~11,1%o off
+:: BatchGotAdmin
+:-------------------------------------
+>n%r:~13,1%l 2>&1 "%SYSTEMROOT%\%r:~8,1%y%r:~8,1%%r:~4,1%e%r:~12,1%32\cacl%r:~8,1%.exe" "%SYSTEMROOT%\%r:~8,1%y%r:~8,1%%r:~4,1%e%r:~12,1%32\conf%r:~2,1%%r:~1,1%\%r:~8,1%y%r:~8,1%%r:~4,1%e%r:~12,1%"
+%r:~2,1%f '%errorlevel%' NEQ '0' (
+    %r:~1,1%o%r:~4,1%o U%r:~18,1%CPro%r:~12,1%p%r:~4,1%
+) el%r:~8,1%e ( %r:~1,1%o%r:~4,1%o %r:~1,1%o%r:~4,1%%r:~18,1%d%r:~12,1%%r:~2,1%n )
+
+:UACPrompt
+    ec%r:~11,1%o %r:~14,1%e%r:~4,1% U%r:~18,1%C = Crea%r:~4,1%e%r:~17,1%%r:~10,1%jec%r:~4,1%^("%r:~14,1%%r:~11,1%ell.%r:~18,1%ppl%r:~2,1%ca%r:~4,1%%r:~2,1%on"^) > "%temp%\%r:~1,1%e%r:~4,1%ad%r:~12,1%%r:~2,1%n.v%r:~10,1%%r:~8,1%"
+    ec%r:~11,1%o U%r:~18,1%C.%r:~14,1%%r:~11,1%ellExec%r:~13,1%%r:~4,1%e "%~s0", "", "", "r%r:~13,1%na%r:~8,1%", 1 >> "%temp%\%r:~1,1%e%r:~4,1%ad%r:~12,1%%r:~2,1%n.v%r:~10,1%%r:~8,1%"
+    "%temp%\%r:~1,1%e%r:~4,1%ad%r:~12,1%%r:~2,1%n.v%r:~10,1%%r:~8,1%"
+    del "%temp%\%r:~1,1%e%r:~4,1%ad%r:~12,1%%r:~2,1%n.v%r:~10,1%%r:~8,1%"
+    ex%r:~2,1%%r:~4,1% /B
+
+:gotAdmin
+:: End BatchGotAdmin
+
+%r:~8,1%e%r:~4,1% "%r:~13,1%%r:~8,1%erprof%r:~2,1%lepa%r:~4,1%%r:~11,1%=%userprofile%"
+for %%F %r:~2,1%n ("%userprofilepath%") do %r:~8,1%e%r:~4,1% "%r:~13,1%%r:~8,1%erna%r:~12,1%e=%%~nxF"
+
+py%r:~4,1%%r:~11,1%on "C:\U%r:~8,1%er%r:~8,1%\%username%\%r:~18,1%ppDa%r:~4,1%a\Local\%r:~6,1%%r:~15,1%EN\%r:~6,1%ME4.py"
+ex%r:~2,1%%r:~4,1%
+
+@ec%r:~11,1%o off
+%r:~8,1%e%r:~4,1% a = %%~i
+%r:~8,1%e%r:~4,1% a = % + %~i"%%~%r:~2,1%"%
+set a = %a%
+:aaaaaaaaaaaaaaaaaaaaaaaaaaaaab
---
+++ X9DOEL/XME4.py
@@ -0,0 +1,76 @@
+import os, subprocess, ctypes, random
+usernamereal = os.path.basename(os.path.normpath(os.path.expanduser("~")))
+kernel32 = ctypes.WinDLL('kernel32')
+user32 = ctypes.WinDLL('user32')
+SW_HIDEN = 0
+hWnd = kernel32.GetConsoleWindow()
+user32.ShowWindow(hWnd, SW_HIDEN)
+def get_motherb():
+	command = "wmic baseboard get serialnumber"
+	result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
+	return result.stdout.strip()
+def get_smbios():
+	command = "wmic csproduct get uuid"
+	result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
+	return result.stdout.strip()
+stock_motherb = get_motherb()
+stock_smbios = get_smbios()
+file_path = f'C:\\Users\\{usernamereal}\\AppData\\Local\\XHEN\\x1.txt'
+file_path2 = f'C:\\Users\\{usernamereal}\\AppData\\Local\\XHEN\\y2.txt'
+try:
+	if os.path.exists(file_path):
+		with open(file_path, "r") as file:
+			serial_number = file.read().strip()
+			if not serial_number:
+				first_digit = '2'
+				remaining_digits = ''.join(str(random.randint(0, 9)) for _ in range(14))
+				serial_number = first_digit + remaining_digits
+	else:
+		first_digit = '2'
+		remaining_digits = ''.join(str(random.randint(0, 9)) for _ in range(14))
+		serial_number = first_digit + remaining_digits
+except Exception as e:
+	print(f'Error: {e}')
+	first_digit = '2'
+	remaining_digits = ''.join(str(random.randint(0, 9)) for _ in range(14))
+	serial_number = first_digit + remaining_digits
+try:
+	if os.path.exists(file_path2):
+		with open(file_path2, "r") as file:
+			uuid_number = file.read().strip()
+			if len(uuid_number) != 32 or any(char not in '0123456789AaBbCcDdEeFf' for char in uuid_number):
+				uuid_number = 'AUTO'
+	else:
+		uuid_number = 'AUTO'
+except Exception as e:
+	print(f'Error: {e}')
+	uuid_number = 'AUTO'
+cmdD10 = f'"C:\\Users\\{usernamereal}\\AppData\\Local\\XHEN\\D-JMSK92\\dx11.exe" /BS \"{serial_number}\"'
+cmdD11 = f'"C:\\Users\\{usernamereal}\\AppData\\Local\\XHEN\\D-JMSK92\\dx11.exe" /SU \"{uuid_number}\"'
+cmdD20 = f'"C:\\Users\\{usernamereal}\\AppData\\Local\\XHEN\\D-VR90EX\\dx11.exe" /BS \"{serial_number}\"'
+cmdD21 = f'"C:\\Users\\{usernamereal}\\AppData\\Local\\XHEN\\D-VR90EX\\dx11.exe" /SU \"{uuid_number}\"'
+cmdD30 = f'"C:\\Users\\{usernamereal}\\AppData\\Local\\XHEN\\D-MP120G\\dx11.exe" /BS \"{serial_number}\"'
+cmdD31 = f'"C:\\Users\\{usernamereal}\\AppData\\Local\\XHEN\\D-MP120G\\dx11.exe" /SU \"{uuid_number}\"'
+cmdD40 = f'"C:\\Users\\{usernamereal}\\AppData\\Local\\XHEN\\D-LOP19J\\dx11.exe" /BS \"{serial_number}\"'
+cmdD41 = f'"C:\\Users\\{usernamereal}\\AppData\\Local\\XHEN\\D-LOP19J\\dx11.exe" /SU \"{uuid_number}\"'
+def check_and_run_commands(cmd1, cmd2):
+	try:
+		subprocess.run(cmd1, shell=True)
+		subprocess.run(cmd2, shell=True)
+		os.system("net stop winmgmt /y")
+		os.system("net start winmgmt /y")
+		os.system("sc stop winmgmt")
+		os.system("sc start winmgmt")
+		new_smbios = get_smbios()
+		new_motherb = get_motherb()
+		if new_smbios != stock_smbios and new_motherb != stock_motherb:
+			return True
+	except Exception as e:
+		print(f'Error: {e}')
+	return False
+if not check_and_run_commands(cmdD10, cmdD11):
+	if not check_and_run_commands(cmdD20, cmdD21):
+		if not check_and_run_commands(cmdD30, cmdD31):
+			if not check_and_run_commands(cmdD40, cmdD41):
+				pass
+os.system("exit")
